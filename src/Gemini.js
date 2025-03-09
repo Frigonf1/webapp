09ff87f990027ad85bcc9ab964353bd711d78445
Francois Frigon <francois.frigon@umontreal.ca>
2025-03-08 20:34:34 -0500

    Caliss

---
+++ src/Gemini.js
@@ -0,0 +1,7 @@
+const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+const prompt = "Explain how AI works";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+createApp(App).mount('#app')
